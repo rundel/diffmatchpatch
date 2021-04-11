d6b9c401fd71ff048384b030af19f5b6a438db42
Colin Rundel <rundel@gmail.com>
2021-04-11 23:02:27 +0100

    Fix printing for non-ansi supporting cases

--- R/print.R
+++ R/print.R
@@ -1,6 +1,6 @@
 dpm_theme = list(
   "EQUAL"  = function(x) { x },
-  "INSERT" = function(x) { cli::style_underline(cli::style_bold(cli::col_green(x))) },
+  "INSERT" = function(x) { cli::style_inverse(cli::style_bold(cli::col_green(x))) },
   "DELETE" = function(x) { cli::style_inverse(cli::style_bold(cli::col_red(x))) }
 )
 
@@ -11,7 +11,17 @@ print.diff_df = function(x, ...) {
     function(row) {
       text = row[1]
       op = row[2]
-      dpm_theme[[op]](text)
+      if (cli::is_ansi_tty()) {
+        dpm_theme[[op]](text)
+      } else if (op == "INSERT") {
+        paste0("+|", text, "|+")
+      } else if (op == "DELETE") {
+        paste0("-|", text, "|-")
+      } else if (op == "EQUAL") {
+        text
+      } else {
+        stop("Unknow op: ", op, call. = FALSE)
+      }
     }
   )
   text = paste(text, collapse="")
